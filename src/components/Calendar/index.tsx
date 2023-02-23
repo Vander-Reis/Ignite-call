1c94b842316ee7b8bf65e5e8cbfb6dfde87c7c90
Vander-Reis <vanderreis2017@outlook.com>
2023-02-23 10:21:06 -0300

    Calendário e disponibilidade - Calculando dias do mês

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,7 +8,7 @@ import {
   CalendarBody,
   CalendarDay,
 } from './styles'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 export function Calendar() {
   const [currentDate, setCurrentDate] = useState(() => {
@@ -24,6 +24,27 @@ export function Calendar() {
     const previousMonthDate = currentDate.add(1, 'month')
     setCurrentDate(previousMonthDate)
   }
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   const shortWeekDays = getWeekDays({ short: true })
 
