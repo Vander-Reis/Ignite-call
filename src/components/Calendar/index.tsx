5a7ce11dc5954cc86c02161c8ecdd588fcc12e39
Vander-Reis <vanderreis2017@outlook.com>
2023-02-25 10:13:42 -0300

    Calendário e disponibilidade - Bloqueando horários no passado

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -113,7 +113,7 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
           disabled:
             date.endOf('day').isBefore(new Date()) ||
             blockedDates.blockedWeekDays.includes(date.get('day')) ||
-            blockedDates.blockedDates.includes(date.get('date')),
+            blockedDates.blockedDates?.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
